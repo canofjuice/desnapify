d54771791b54ebbe9ef7459c5125254a492b7e57
Compact <135176122+canofjuice@users.noreply.github.com>
2024-09-11 22:16:10 +0200

    Add files via upload

---
+++ desnapify.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+if [[ $(readlink -f /proc/$(ps -o ppid:1= -p $$)/exe) != $(readlink -f "$SHELL") ]]
+then 
+zenity --info --title="Welcome to De-Snapify!" --text="<big>This script will automate the process of removing and disabling snaps, and replacing the snap version of Firefox with a local package version from Mozilla's PPA's.</big>" --width=600
+zenity --warning --text="<big>WARNING: Snaps are a important part of the Ubuntu (and all of it's flavors, except Lubuntu) experience.\n\nI am NOT responsible for any damage caused by the script or ANYTHING done by the script in general.\n\nUse this at your own risk.</big>" --title=De-Snapify --width=800
+zenity --question --text="<big>Proceed with removing snaps?</big>" --title=De-Snapify --width=400
+if [ $? = 0 ] ; then
+sudo snap remove firefox
+sudo systemctl disable --now snapd
+sudo systemctl mask snapd
+sudo apt purge snapd -y
+sudo apt-mark hold snapd
+rm -rf ~/snap/
+sudo rm -rf /snap
+sudo rm -rf /var/snap
+sudo rm -rf /var/lib/snapd
+sudo cat <<EOF | sudo tee /etc/apt/preferences.d/nosnap.pref
+Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+EOF
+sudo apt update -y
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1' | sudo tee /etc/apt/preferences.d/mozilla
+
+sudo apt update && sudo apt install firefox -y
+zenity --info --title=De-Snapify --text="DONE! Snaps have been removed, disabled and the Mozilla PPA version of Firefox has been installed.\n\n Please restart or re-login and enjoy your snap-less Ubuntu experience!" --width=500
+else
+exit
+fi
+else
+zenity --error --title=De-Snapify --text="<big>Please run the script from a terminal in order to catch any errors that occur during the execution of the script.</big>"
+fi
